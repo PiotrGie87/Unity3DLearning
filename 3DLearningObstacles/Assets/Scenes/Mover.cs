2303c794bcdc6d0b12cae2fb6f5996a810abe856
PiotrGie87 <piotr.gasowski87@gmail.com>
2021-04-13 17:52:58 +0200

    Section2 Lesson13

--- 3DLearningObstacles/Assets/Scenes/Mover.cs
+++ 3DLearningObstacles/Assets/Scenes/Mover.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    [SerializeField] float xValue = 0.01f;
-    [SerializeField] float yValue = 0.01f;
-    [SerializeField] float zvalue = 0.01f;
+    
+    
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +16,8 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0, yValue, 0);
+        float xValue = Input.GetAxis("Horizontal");
+        float zValue = Input.GetAxis("Vertical");
+        transform.Translate(xValue,0,zValue);
     }
 }
